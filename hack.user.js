2d77a78e88aea43066fa010ade4e2d3952c034e4
Matthijs van Henten <mvhenten@tty.nl>
2011-12-09 11:33:46 +0100

    more robust

--- hack.user.js
+++ hack.user.js
@@ -15,14 +15,15 @@ function scriptTag(src, callback) {
 
         if (!callback.done && (!state || /loaded|complete/.test(state))) {
             callback.done = true;
-            callback();
+            var script = document.createElement("script");
+            script.textContent = "(" + callback.toString() + ")();";
+            ( document.body || head ).appendChild( script );
         }
     };
 
     // use body if available. more safe in IE
     (document.body || head).appendChild(s); 
 }
-
 
 scriptTag('https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', function(){
     scriptTag( 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js', function(){
