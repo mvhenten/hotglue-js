1b67bfc3165b1a688c7e127b4ce3920bdc3e59b2
Matthijs van Henten <matthijs@ischen.nl>
2012-01-03 00:17:36 +0100

    use new inline plugin

--- glue.js
+++ glue.js
@@ -1,70 +1,92 @@
 var glue_me = (function(){
     var selectors = 'a:visible,p,h1,h2,h3,h4,h5';
-    var target    = 'http://hotglue2.localhost/jsonp.php';
+    console.log($(selectors).collectCSS());
 
-    $(document.body).append('<img onclick="glue_me.now()" id="glueme" src="https://github.com/mvhenten/hotglue2/raw/master/img/hotglue-logo.png" style="position:absolute; top:10%; right:10%; z-index:999999; cursor:pointer;" alt="hotglue me">');
-    $('#glueme').draggable();
+    $(document.body).append(
+        $('<img onclick="glue_me.now()" id="glueme" '
+          +'src="https://github.com/mvhenten/hotglue2/raw/master/img/hotglue-logo.png" '
+          + 'style="position:absolute; top:10%; right:10%; z-index:999999; cursor:pointer; " '
+          +'alt="hotglue me">').hide()
+    );
 
+
+    $('#glueme').draggable().fadeIn(1000);
 
     return {
-        now: function(){
-            var self = this;
+        collect: function(){
+            this.sanitizeImages();
+
+            var collection = $( $(selectors).collectCSS() ).map(function(i, obj ){
+                var offset = $(el).offset();
+
+                obj.style.top  = offset.top + 'px';
+                obj.style.left = offset.left + 'px';
+
+                $(obj.element).find('*').inlineCSS();
+
+                obj.tagName = obj.element.tagName.toLowerCase();
+
+                switch( obj.tagName ){
+                    case 'img':
+                        obj.src  = obj.element.src;
+                        break;
+                    case 'a':
+                        obj.href = obj.element.href;
+                        break;
+                }
+
+                return obj;
+            });
+
+            //this.save( collection );
+        },
+
+        sanitizeImages: function(){
             $('img').each(function( i, el ){
                 $(el).attr('src', el.src );
             });
-
-            $(selectors).each( function(i, el){
-                var offset = $(el).offset();
-                var position = $(el).position();
-                self.glue(el);
-            });
-        },
-        glue: function( el ){
-            var offset = $(el).offset();
-            var css = {
-                'z-index': 100,
-                width: $(el).width() + 'px',
-                height: $(el).height() + 'px',
-                top: offset.top + 'px',
-                left: offset.left + 'px',
-                'background-color': $(el).css('background-color'),
-                'color': $(el).css('color'),
-                'line-height': $(el).css('line-height'),
-                'font-family': $(el).css('font-family'),
-                'font-size': $(el).css('font-size'),
-                'font-style':$(el).css('font-style'),
-                'font-weight':$(el).css('font-weight')
-            }
-
-            $(el).find('*').inlineCSS();
-
-            this.save({
-                css: css,
-                content: el.innerHTML
-            });
-        },
-        save: function( el_data ){
-            $.getJSON( target + "?action=create&callback=?",
-                function(data) {
-                    var id = parseInt(data['#data'].name.split('.').pop());
-                    $.getJSON( target + "?&callback=?",{
-                        action: 'save',
-                        id: id,
-                        css: JSON.stringify( el_data.css ),
-                        content: el_data.content
-                    },
-                    function(data){
-                        //console.log(data);
-                });
-            });
-        },
-        absolutize: function(el){
-            //    $(document.body).append( el.parentNode.removeChild(el) );
-            $(el).css({
-                position: 'absolute',
-                top: css.top + 'px',
-                left: css.top + 'px'
-            });
         }
-    };
+    }
+
+
+
+    //var target    = 'http://hotglue2.localhost/jsonp.php';
+    //
+
+    //
+    //
+    //return {
+    //    now: function(){
+    //        var self = this;
+    //
+    //        $(selectors).each( function(i, el){
+    //            var offset = $(el).offset();
+    //            var position = $(el).position();
+    //            self.glue(el);
+    //        });
+    //    },
+    //    save: function( el_data ){
+    //        $.getJSON( target + "?action=create&callback=?",
+    //            function(data) {
+    //                var id = parseInt(data['#data'].name.split('.').pop());
+    //                $.getJSON( target + "?&callback=?",{
+    //                    action: 'save',
+    //                    id: id,
+    //                    css: JSON.stringify( el_data.css ),
+    //                    content: el_data.content
+    //                },
+    //                function(data){
+    //                    //console.log(data);
+    //            });
+    //        });
+    //    },
+    //    absolutize: function(el){
+    //        //    $(document.body).append( el.parentNode.removeChild(el) );
+    //        $(el).css({
+    //            position: 'absolute',
+    //            top: css.top + 'px',
+    //            left: css.top + 'px'
+    //        });
+    //    }
+    //};
 })();
